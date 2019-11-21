28ffe124229dcbd6a5fd4a1a7168ce926f84cee1
Aleksandr <aleksandr.v.dikarev@gmail.com>
2019-11-21 12:51:39 +0400

    Update uWAVE_Example_2.ino

--- uWAVE_Example_2.ino
+++ uWAVE_Example_2.ino
@@ -1,3 +1,28 @@
+/* 
+ *   Necessary hardware:
+ *      - Arduino UNO board
+ *      - uWAVE underwater acoustic modem
+ *      
+ *   Connect uWAVE's TX wire to Arduino's PIN 2
+ *   Connect uWAVE's RX wire to Arduino's PIN 3
+ *   Connect uWAVE's CMD wire to Arduino's PIN 4
+ *   Connect uWAVE's GND wire to any Arduino's GND PIN
+ *   Connect uWAVE's VCC wire to Arduino's 5V PIN
+ *   
+ *   Connect Arduino board to a PC with USB cable
+ *   
+ *   If you have another uWAVE modem, turn it on and make sure its tx and rx channels IDs set to 0
+ *   
+ *   Open Arduino IDE
+ *   Open this scketch in the IDE
+ *   Open Port Monitor in Main menu -> Tools -> Port monitor
+ *   
+ *   Program your board
+ *   
+ *   Now you can use Arduino IDE's port monitor as a Chat terminal to chat with another uWAVE modem,
+ *   which is suppose to be connected either the same way with another Arduino board or via UART-USB converter
+ *   to a PC with any terminal software intalled.
+ */
 
 #include <SoftwareSerial.h>
 
